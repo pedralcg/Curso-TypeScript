141827dae88075dfc00929c98b980bcae0d85a5b
Pedro Alcoba <pedralcg@gmail.com>
2025-03-27 23:59:28 +0100

    V01 Introduccion TypeScript

---
+++ build/index.js
@@ -0,0 +1,8 @@
+"use strict";
+console.log("Hola Typescript");
+console.log("Adiós, Pedro");
+// Esto es un comentario en TS
+/**
+ * Esto es un comentario
+ * Multilinea
+ */ 
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+console.log("Hola Typescript");
+console.log("Adiós, Pedro")
+
+// Esto es un comentario en TS
+
+/**
+ * Esto es un comentario
+ * Multilinea
+ */
